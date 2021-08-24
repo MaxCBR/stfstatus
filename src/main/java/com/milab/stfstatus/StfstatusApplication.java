cc5e624146c995c5e81e0547af1a0c50ca2487c2
MaxCBR <geep1988@gmail.com>
2021-08-24 21:23:32 +1000

    first commit

---
+++ src/main/java/com/milab/stfstatus/StfstatusApplication.java
@@ -0,0 +1,13 @@
+package com.milab.stfstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StfstatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StfstatusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milab/stfstatus/StfstatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milab.stfstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StfstatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
